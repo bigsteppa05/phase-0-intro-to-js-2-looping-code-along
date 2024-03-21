ea68a8a0c8f09d8c7cab17ef0ddfad22e8889ad4
bigsteppa05 <habibforderiv05@gmail.com>
2024-03-21 18:21:36 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(names, surprise) {
+    let output = [];
+    for (let i = 0; i < names.length; i++) {
+        output.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return output;
+}
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
